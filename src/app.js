ecd38e19e79e0c81dd2203e5c8dae83788d1ff72
Cristie Huang <ch746@cornell.edu>
2020-10-03 23:31:57 -0400

    complete landing page

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = ''; // Your client id
-var client_secret = ''; // Your secret
+var client_id = 'df9043305ece4ffca058fc21df83e5ea'; // Your client id
+var client_secret = 'a780f955e5f849fe98478aeabef4f716'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
