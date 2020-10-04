e32935fb4b45b05e772c089ca77ae77fdd16dc92
charusm18 <charusm18@gmail.com>
2020-10-04 13:13:14 -0400

    integrated front end with dictionary

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ var cookieParser = require('cookie-parser');
 const { json, Router } = require('express');
 
 var client_id = 'df9043305ece4ffca058fc21df83e5ea'; // Your client id
-var client_secret = '4a5de4500fb84215b007a61977734c8f'; // Your secret
+var client_secret = '5a7dc1a2891c4776a07e4b15ee8d82a6'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 var user_info = {}; //dictionary that holds user information
